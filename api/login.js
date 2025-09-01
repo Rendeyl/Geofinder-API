aa1309f82c336d553ce8a7d2df35b13ce65375f1
RenDeyl <rendey73@gmail.com>
2025-09-01 14:13:02 +0800

    v1.5

--- api/login.js
+++ api/login.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://anonymessagex.vercel.app",   
+  origin: "https://geofinder-rd.vercel.app/",   
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
