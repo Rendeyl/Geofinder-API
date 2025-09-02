a58a64cd81693ecf62916d4977b3b576d9e598f1
RenDeyl <rendey73@gmail.com>
2025-09-02 22:38:15 +0800

    v2.4.6

--- api/history.js
+++ api/history.js
@@ -47,7 +47,7 @@ app.get("/api/history", async (req, res) => {
 });
 
 app.delete("/api/history", async (req, res) => {
-  const { id } = req.query.id;
+  const id = req.query.id;
 
   try {
     const [result] = await pool.query("DELETE FROM history WHERE id = ?", [id]);
